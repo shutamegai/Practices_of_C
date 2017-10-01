498ec7585b4ecce330e6fb7ebffbb924610af43c
Shu Tamegai <tamegaishu1995@gmail.com>
2017-10-01 13:48:43 +0900

    5th commit

---
+++ section_4/numeral.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+        printf("%f円\n", 100.0/3);
+        return 0;
+        
+}
---
+++ section_5/variable.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void){
+        int value;
+        value = 123;
+        printf("%d\n", value);
+        return 0;
+}
