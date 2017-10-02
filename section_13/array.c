39711db6624b0a7a1d10a9a8661dcb2984eb4efc
Shu Tamegai <tamegaishu1995@gmail.com>
2017-10-02 09:08:13 +0900

    13th commit

--- section_13/array.c
+++ section_13/array.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
+#include <memory.h>
+
 
 int main(void) {
-     int array[] = {12, 23, 34, 45};
-     int array_count = sizeof(array)/sizeof(array[0]);
+     int array1[] = {12, 23, 34, 45};
+     int array1_count = sizeof(array1)/sizeof(array1[0]);
+     int array2[] = {11, 22, 33, 44, 55};
+     int array2_count = sizeof(array2)/sizeof(array2[0]);
      int i;
      
-     for (i=0; i<array_count; i++) {
-          printf("array[%d] = %d\n", i, array[i]);
+     for (i=0; i<array1_count; i++) {
+          printf("array1[%d] = %d\n", i, array1[i]);
+     }
+
+     memcpy(array2, array1, array1_count);
+     
+     for (i=0; i<array2_count; i++) {
+          printf("array2[%d] = %d\n", i, array2[i]);
      }
      
      return 0;
